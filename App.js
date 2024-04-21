a0a890320754e43ce4de19be937bcf98f112c4de
ChiThienKaka <thien190602@gmail.com>
2024-04-21 23:23:07 +0700

    thuchanhlap2LT

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Drawer, PaperProvider } from 'react-native-paper';
+import HomeScreen from './Screens/HomeScreen';
+import DetailsScreen from './Screens/DetailsScreen';
+import CustomNavigationBar from './CustomNavigationBar';
+import MyDrawer from './Screens/Drawer';
+import ButtonTabs from './Screens/ButtonTab';
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <PaperProvider>
+      <NavigationContainer>
+      {/* <MyDrawer></MyDrawer> */}
+      <ButtonTabs/>
+        {/* <Stack.Navigator initialRouteName="Home" screenOptions={{header:(props)=><CustomNavigationBar {...props}/>}}>
+              <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+              <Stack.Screen name="Details" component={DetailsScreen}></Stack.Screen>
+        </Stack.Navigator> */}
+      </NavigationContainer>
+   </PaperProvider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
